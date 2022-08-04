5c6effb9b8e55028f5eb8be139b8288ec1769a0a
AFO199690 <110529890+AFO199690@users.noreply.github.com>
2022-08-04 19:45:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import Shape.Square;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//create object of square class
+  Square sqt =new Square()
+  //call method of shape class using square class object
+  sqt.sha();
+ //call method of Rectangle class using square class object
+  sqt.print_rect();
+	
+}
+
+}
+
+
+
